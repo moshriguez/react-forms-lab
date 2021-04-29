7923676d39917e08ed67c72eff524b9a4a478dd9
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-29 11:53:40 -0700

    commit tree 6c20db2d8d0fd4ece3dd020ceb0862d30bde055a

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,25 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      message: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" 
+        onChange={event => {this.handleChange(event)}}
+        value={this.state.message}/>
+        <span>{this.props.maxChars - this.state.message.length} characters left</span>
       </div>
     );
   }
